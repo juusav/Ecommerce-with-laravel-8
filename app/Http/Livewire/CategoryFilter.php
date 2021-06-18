a339b8b4c3a79550250894aa8a4cbe93e1a58b22
jusav <justicevgomez001@gmail.com>
2021-06-18 22:21:05 +0200

    search filter v2

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -12,9 +14,34 @@ class CategoryFilter extends Component
     public $category, $subcategoria, $marca;
     
     public function render(){
-        $products = $this->category->products()
-                    ->where('status', 2)
-                    ->paginate(20);
+        // $products = $this->category->products()
+        //             ->where('status', 2)
+        //             ->paginate(20);
+
+        // He separado la consulta y la coleccion para poder realizar consultas dinamicas 
+        // CONSULTA
+        $productsQuery = Product::query()->WhereHas('subcategory.category', function(Builder $query){
+            $query->where('id', $this->category->id); //id que coincida con el id de la categoria
+            //Ya que no hay una relación directa de productos a categoria usé el...
+            //intermediario que es subcategoria para poder llegar a categoria
+
+        });
+
+        // Si tengo agregado algo almacenado en subcategoria agregará un filtro a la relación subcategory y mostrará los productos relacionados a dichas subcategorias
+        if ($this->subcategoria) {
+            $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
+                $query->where('name', $this->subcategoria);
+            }); //Este filtro se aplicará solamente si hay algo en la propiedad subcategory
+        }
+        if ($this->marca) {
+            $productsQuery = $productsQuery->whereHas('brand', function(Builder $query){
+                $query->where('name', $this->marca);
+            });
+        }
+
+        // COLECCIÓN
+        $products = $productsQuery->paginate(20);
+
 
         return view('livewire.category-filter', compact('products'));
     }
