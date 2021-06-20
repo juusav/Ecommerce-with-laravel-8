ca49b74ecefdcf0241754bfc3ada0e687c72ba70
jusav <justicevgomez001@gmail.com>
2021-06-20 22:28:18 +0200

    enable disable buttons

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -6,7 +6,13 @@ use Livewire\Component;
 
 class AddCartItem extends Component
 {
+    public $product, $quantity;
     public $qty = 1;
+
+    public function mount(){
+        $this->quantity = $this->product->quantity;
+    }
+
     public function decrement(){
         $this->qty = $this->qty - 1;
     }
--- resources/views/livewire/add-cart-item.blade.php
+++ resources/views/livewire/add-cart-item.blade.php
@@ -1,15 +1,25 @@
-<div>
+<div x-data>{{--  Habilita Alpine --}}
+
+    <p class="text-gray-500 mb-4">
+        <span class="font-semibold">En Stock: </span>{{$quantity}}
+    </p>
+    
     <div class="flex">
-        <div>
-            <x-button wire:click="decrement">
+        <div class="mr-4">
+            <x-button 
+                disabled 
+                x-bind:disabled="$wire.qty <= 1"{{-- el button se habilita si el número es mayor o igual que uno --}}
+                wire:loading.attr="disabled"{{-- Mientras se está cargando el boton estará deshabilitado --}}
+                wire:target="decrement"{{-- El boton solo se deshabilita cuando ejecutemos este método  --}}
+                wire:click="decrement">{{-- Variable en componente AddCartItem --}}
                 -
             </x-button>
-        </div>
-
-        <span class="mx-3 text-gray-700">{{$qty}}</span>
-
-        <div class="mr-4">
-            <x-button wire:click="increment">
+            <span class="mx-3 text-gray-700">{{$qty}}</span>
+            <x-button 
+                x-bind:disabled="$wire.qty >= $wire.quantity"{{-- Se bloquea cuando la cantidad es igual que la cantidad de los productos en stock  --}}
+                wire:loading.attr="disabled"
+                wire:target="increment"
+                wire:click="increment">
                 +
             </x-button>
         </div>
