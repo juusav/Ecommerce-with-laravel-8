c29a695d4e445b4bdfffce098895b99392b59f33
jusav <justicevgomez001@gmail.com>
2021-06-22 16:20:29 +0200

    sizes stock

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -8,11 +8,22 @@ use Livewire\Component;
 class AddCartItemSize extends Component
 {
     public $product, $sizes;
+
+    public $color_id = "";
+    public $qty = 1;
+    public $quantity = 0;
     public $size_id = "";
+
+
     public $colors = [];
+
     public function updatedSizeId($value){
         $size = Size::find($value);
         $this->colors = $size->colors;
+    }
+    public function updatedColorId($value){
+        $size = Size::find($this->size_id);
+        $this->quantiy = $size->colors->find($value)->pivot->quantity;
     }
     
     public function mount(){
--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -1,23 +1,52 @@
-<div>
+<div x-data>
     <div>
         <p class="text-lg text-gray-700">Talla:</p>
-
         <select wire:model="size_id" class="form-control w-full">
-            <option value="" selected disabled>Seleccione una talla</option>
+            <option value="" selected disabled>Seleccionar talla</option>
             @foreach ($sizes as $size)
                 <option value="{{$size->id}}">{{$size->name}}</option>
             @endforeach
         </select>
     </div>    
 
-    <div>
-        <p class="text-lg text-gray-700">Color:</p>
 
-        <select class="form-control w-full">
+    <div class="mt-3">
+        <p class="text-xl text-gray-700">Color:</p>
+        <select wire:model="color_id" class="form-control w-full">
             <option value="" selected disabled>Seleccionar color</option>
             @foreach ($colors as $color)
                 <option class="capitalize" value="{{$color->id}}">{{__($color->name)}}</option>
             @endforeach
         </select>
     </div>    
+
+    <div class="flex mt-3">
+        <div class="mr-4">
+            <x-button 
+                disabled 
+                x-bind:disabled="$wire.qty <= 1"{{-- el button se habilita si el número es mayor o igual que uno --}}
+                wire:loading.attr="disabled"{{-- Mientras se está cargando el boton estará deshabilitado --}}
+                wire:target="decrement"{{-- El boton solo se deshabilita cuando ejecutemos este método  --}}
+                wire:click="decrement">{{-- Variable en componente AddCartItem --}}
+                -
+            </x-button>
+            <span class="mx-3 text-gray-700">{{$qty}}</span>
+            <x-button 
+                x-bind:disabled="$wire.qty >= $wire.quantity"{{-- Se bloquea cuando la cantidad es igual que la cantidad de los productos en stock  --}}
+                wire:loading.attr="disabled"
+                wire:target="increment"
+                wire:click="increment">
+                +
+            </x-button>
+        </div>
+
+        <div class="flex-1">
+            <x-button 
+                x-bind:disabled="!$wire.quantity"
+                class="full" 
+                color="blue">
+                Agregar al carrito de compras
+            </x-button>
+        </div>
+    </div>
 </div>
