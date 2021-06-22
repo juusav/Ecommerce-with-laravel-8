2110ffee6ea7dd6c4132370f96d4e8e34c1f215a
jusav <justicevgomez001@gmail.com>
2021-06-22 15:21:51 +0200

    options of color and size in add-cart-item-size view

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Size;
 use Livewire\Component;
 
 class AddCartItemSize extends Component
 {
-    public function render()
-    {
+    public $product, $sizes;
+    public $size_id = "";
+    public $colors = [];
+    public function updatedSizeId($value){
+        $size = Size::find($value);
+        $this->colors = $size->colors;
+    }
+    
+    public function mount(){
+        $this->sizes = $this->product->sizes;
+    }
+
+    public function render(){
         return view('livewire.add-cart-item-size');
     }
 }
--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div>
+        <p class="text-lg text-gray-700">Talla:</p>
+
+        <select wire:model="size_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione una talla</option>
+            @foreach ($sizes as $size)
+                <option value="{{$size->id}}">{{$size->name}}</option>
+            @endforeach
+        </select>
+    </div>    
+
+    <div>
+        <p class="text-lg text-gray-700">Color:</p>
+
+        <select class="form-control w-full">
+            <option value="" selected disabled>Seleccionar color</option>
+            @foreach ($colors as $color)
+                <option class="capitalize" value="{{$color->id}}">{{__($color->name)}}</option>
+            @endforeach
+        </select>
+    </div>    
 </div>
