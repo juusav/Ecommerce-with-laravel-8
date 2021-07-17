ce11807c36d70b7ad9a3ed854fd655be4512b351
justice <justicevgomez001@gmail.com>
2021-07-17 16:33:19 +0200

    updated create order view

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Department;
 use Livewire\Component;
 
 class CreateOrder extends Component
 {
-    public function render()
-    {
+    public $address, $reference;    
+    public $departments, $cities = [], $districts = [];
+    public $department_id = "", $city_id = "", $district_id = "";
+
+    public function mount(){
+        $this->departments = Department::all();
+    }
+
+    public function render(){
         return view('livewire.create-order');
     }
 }
--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -1,43 +1,93 @@
 <div class="container py-8 grid grid-cols-5 gap-6">
     <div class="col-span-3">
+        {{-- Contácto --}}
         <div class="bg-white rounded-lg shadow p-6">
             <div class="mb-4">
-                <x-jet-label value="nombre de contácto" />
+                <x-jet-label value="Nombre de contácto" />
                 <x-jet-input type="text" placeholder="Ingrese el nombre de la persona que recibirá el producto"
                     class="w-full" />
             </div>
-
             <div>
-                <x-jet-label value="nombre de contácto" />
+                <x-jet-label value="Teléfono de contácto" />
                 <x-jet-input type="text" placeholder="Ingrese el número de teléfono de contácto" class="w-full" />
             </div>
         </div>
 
-        <div>
+        {{-- Envíos --}}
+        <div x-data="{ envio_type: 1 }">
             <p class="mt-6 mb-3 text-lg text-gray-700 font-semibold">Envíos</p>
             <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center mb-4">
-                <input type="radio" name="envio" class="text-gray-600">
+                <input type="radio" x-model="envio_type" value="1" name="envio_type" class="text-gray-600">
                 <span class="ml-2 text-gray-700">Recoger en tienda (calle misteriosa 43, 3 3)</span>
                 <span class="font-semibold text-gray-700 ml-auto">Gratis</span>
             </label>
 
-            <p class="mt-6 mb-3 text-lg text-gray-700 font-semibold">Envíos</p>
-            <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center">
-                <input type="radio" name="envio" class="text-gray-600">
-                <span class="ml-2 text-gray-700">Entrega a domicilio</span>
-            </label>
+            <div class="bg-white rounded-lg shadow">
+                <label class="px-6 py-4 flex items-center">
+                    <input type="radio" x-model="envio_type" value="2" name="envio_type" class="text-gray-600">
+                    <span class="ml-2 text-gray-700">Entrega a domicilio</span>
+                </label>
+
+                <div class="px-6 pb-6 grid grid-cols-2 gap-6 hidden" :class="{ 'hidden': envio_type !=2 }">
+                    {{-- Departments --}}
+                    <div>
+                        <x-jet-label value="Departmento"/>
+                        <select class="form-control w-full" wire:model="department_id">
+                            <option value="" disabled selected>Selecciona un departamento</option>
+                            @foreach ($departments as $department)
+                                <option value="{{$department->id}}">{{$department->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    {{-- Cities --}}
+                    <div>
+                        <x-jet-label value="Ciudad"/>
+                        <select class="form-control w-full" wire:model="city_id">
+                            <option value="" disabled selected>Selecciona una ciudad</option>
+                            @foreach ($cities as $city)
+                                <option value="{{$city->id}}">{{$city->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    {{-- Districts --}}
+                    <div>
+                        <x-jet-label value="Distrito"/>
+                        <select class="form-control w-full" wire:model="district_id">
+                            <option value="" disabled selected>Selecciona un distrito</option>
+                            @foreach ($districts as $district)
+                                <option value="{{$district->id}}">{{$district->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    {{-- Dirección --}}
+                    <div>
+                        <x-jet-label value="Dirección"/>
+                        <x-jet-input class="w-full" wire:model="address" type="text"/>
+                    </div>
+                    {{-- Referencia --}}
+                    <div class="col-span-2">
+                        <x-jet-label value="Referencia"/>
+                        <x-jet-input class="w-full" wire:model="reference" type="text"/>
+                    </div>
+                </div>
+            </div>
         </div>
 
+        {{-- Continuar con la compra --}}
         <div>
             <x-jet-button class="mt-6 mb-4">Continuar con la compra</x-jet-button>
             <hr>
             <p class="text-sm text-gray-700 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quasi incidunt deleniti temporibus, provident eos reiciendis repudiandae distinctio sapiente iste, neque optio esse sunt eum quisquam. Sit omnis ea cupiditate? <a href="" class="font-semibold text-gray-500">Políticas y privacidad</a> </p>
         </div>
-
     </div>
 
+    {{-- Productos seleccionados --}}
     <div class="col-span-2">
         <div class="bg-white rounded-lg shadow p-6">
+            {{-- Productos seleccionados --}}
             <ul>
                 @forelse (Cart::content() as $item) 
 
@@ -68,6 +118,7 @@
                 @endforelse
             </ul>
             <hr class="mt-4 mb-3">
+            {{-- Total --}}
             <div class="text-gray-700">
 
                 <p class="flex justify-between items-center">
