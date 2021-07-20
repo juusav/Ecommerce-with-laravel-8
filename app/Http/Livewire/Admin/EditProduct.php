74237ffb90bd198108d8c75039bec9234e4d8ba1
justice <justicevgomez001@gmail.com>
2021-07-20 17:56:24 +0200

    form to edit products

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -2,16 +2,44 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class EditProduct extends Component{
 
-    public $product;
+    public $product, $categories, $subcategories, $brands;
+    public $category_id;
+
+    protected $rules = [
+        'category_id' => 'required',
+        'product.subcategory_id' => 'required',
+        'product.brand_id' => 'required',
+        'product.name' => 'required',
+        'product.price' => 'required',
+        'product.description' => 'required',
+        'product.slug' => 'required|unique:products',
+    ];
 
     public function mount(Product $product){
         $this->product = $product;
+        $this->categories = Category::all();
+        $this->category_id = $product->subcategory->category->id;
+
+        $this->subcategories = Subcategory::where('category_id', $this->category_id)->get(); 
+        $this->brands = Brand::whereHas('categories', function(Builder $query){
+            $query->where('category_id', $this->category_id);
+        })->get();
     }
+
+    // Propiedad computada
+    public function getSubcategoryProperty(){
+        return Subcategory::find($this->product->subcategory_id);
+    }
+
 
     public function render(){
         return view('livewire.admin.edit-product')->layout('layouts.admin');
--- resources/views/livewire/admin/edit-product.blade.php
+++ resources/views/livewire/admin/edit-product.blade.php
@@ -1,3 +1,122 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12 text-gray-700">
+    <h1 class="text-3xl text-center font-semibold mb-8">Crear un producto</h1>
+
+    <div class="grid grid-cols-2 gap-6 mb-4">
+        {{-- Category --}}
+        <div>
+            <x-jet-label value="Categorías" />
+            <select class="w-full form-control" wire:model="category_id">
+                <option value="" selected disabled >Seleccionar categoría</option>
+
+                @foreach ($categories as $category)
+                    <option value="{{$category->id}}">{{$category->name}}</option>
+                @endforeach
+            </select>
+
+            <x-jet-input-error for="category_id" />
+        </div>
+        {{-- Subcategory --}}
+        <div>
+            <x-jet-label value="Subcategorías" />
+            <select class="w-full form-control" wire:model="product.subcategory_id">
+                <option value="" selected disabled >Seleccionar subcategoría</option>
+
+                @foreach ($subcategories as $subcategory)
+                    <option value="{{$subcategory->id}}">{{$subcategory->name}}</option>
+                @endforeach
+            </select>    
+
+            <x-jet-input-error for="product.subcategory_id" />
+        </div>
+    </div>
+
+    {{-- Product name --}}
+    <div class="mb-4">
+        <x-jet-label value="Nombre" />
+        <x-jet-input type="text" 
+            wire:model="product.name"
+            class="w-full" 
+            placeholder="Ingrese el nombre del producto" />
+
+        <x-jet-input-error for="product.brand" />
+    </div>
+    {{-- Slug name --}}
+    <div class="mb-4">
+        <x-jet-label value="Slug" />
+        <x-jet-input type="text" 
+            disabled
+            wire:model="product.slug"
+            class="w-full bg-gray-200" 
+            placeholder="Ingrese el slug del producto" />
+
+        <x-jet-input-error for="product.slug" />
+    </div>
+
+    {{-- Description --}}
+    <div class="mb-4">
+        {{-- El metodo name está renderizando la página cada vez que surge un cambio y este div se ve afectado. wire:ignore ignora ese proceso --}}
+        <div wire:ignore>
+            <x-jet-label value="Descripción" />
+            <textarea rows="4" 
+                class="w-full form-control"
+                wire:model="product.description"
+                x-data
+                x-init="ClassicEditor.create($refs.miEditor)
+                .then(function(editor){
+                    editor.model.document.on('change:data', () => {
+                        @this.set('description', editor.getData()) {{-- Livewire tiene .set que cambia el valor del componente de livewire. Para acceder desde JavaScript hay que agregar @this.set. Primer parametro será el description y este se cambiará por lo que se haya escrito en el editor --}}
+                    })
+                })
+                .catch( error => {
+                    console.error( error );
+                } );"
+                x-ref="miEditor" {{-- Es como un id --}}>
+            </textarea>
+        </div>
+        
+        <x-jet-input-error for="product.description" />
+    </div>
+
+    <div class="grid grid-cols-2 gap-6 mb-4">
+        {{-- Brand --}}
+        <div>
+            <x-jet-label value="Marca" />
+            <select class="form-control w-full" wire:model="product.brand_id">
+                <option value="" selected disabled>Seleccionar una marca</option>
+                @foreach ($brands as $brand)
+                    <option value="{{$brand->id}}">{{$brand->name}}</option>
+                @endforeach
+            </select>
+            <x-jet-input-error for="product.brand_id" />
+        </div>
+        {{-- Price --}}
+        <div>
+            <x-jet-label value="Precio" />
+            <x-jet-input type="number" 
+                wire:model="product.price"
+                class="w-full" 
+                step=".01" />
+            <x-jet-input-error for="product.price" />
+        </div>
+    </div>
+
+    @if (!$this->subcategory->color && !$this->subcategory->size)
+        <div>
+            <x-jet-label value="Cantidad" />
+            <x-jet-input type="number" 
+                wire:model="quantity"
+                class="w-full" />
+            <x-jet-input-error for="quantity" />
+        </div>
+    @endif
+
+    <div class="flex mt-4">
+        <x-jet-button 
+            wire:loading.attr="disabled"
+            wire:target="save"{{-- No interrumpe ningún otro proceso  --}}
+            wire:click="save"
+            class="ml-auto">
+            Crear producto
+        </x-jet-button>
+    </div>
 </div>
