5198721c3a82ace9144fb8a4dae6661864df5120
justice <justicevgomez001@gmail.com>
2021-07-21 14:18:50 +0200

    add size to products

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Size;
 use Livewire\Component;
 
-class SizeProduct extends Component
-{
-    public function render()
-    {
-        return view('livewire.admin.size-product');
+class SizeProduct extends Component{
+
+    public $product, $name, $open = "false";
+    public $name_edit;
+    
+    protected $rules=[
+        'name' => 'required'
+    ];
+
+    public function save(){
+        $this->validate();  
+
+        $this->product->sizes()->create([
+            'name' => $this->name
+        ]);
+        $this->product = $this->product->fresh();
+    }
+
+    public function edit(Size $size){
+        $this->open = true;
+        $this->name_edit = $size->name;
+    }
+
+    public function render(){
+        $sizes = $this->product->sizes;
+        return view('livewire.admin.size-product', compact('sizes'));
     }
 }
--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -1,3 +1,70 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <div class="bg-white shadow-lg rounded-lg p-6 mt-12">
+
+        <div>
+            <x-jet-label>
+                Cantidad
+            </x-jet-label>
+
+            <x-jet-input wire:model="name" type="text" placeholder="Ingresar talla" class="w-full mt-2" />
+            <x-jet-input-error for="name" />
+        </div>
+
+        <div class="flex justify-end items-center mt-4">
+            <x-jet-button
+                wire:click="save"
+                wire:loading.attr="disabled"
+                wire:target="save">
+                Agregar
+            </x-jet-button>
+        </div>
+    </div>
+
+    <ul class="mt-12 space-y-4">
+        @foreach ($sizes as $size)
+            <li class="bg-white shadow-lg rounded-lg p-6" wire:key="size-{{$size->id}}">
+                <div class="flex items-center">
+                    <span class="text-xl font-medium">{{$size->name}}</span>
+
+                    {{-- Edit and delete buttons --}}
+                    <div class="ml-auto">
+                        <x-jet-button 
+                            wire:click="edit({{$size}})"
+                            wire:loading.attr="disabled"
+                            wire:target="edit({{$size}})">
+                            <i class="fas fa-edit"></i>
+                        </x-jet-button>
+
+                        <x-jet-danger-button>
+                            <i class="fas fa-trash"></i>
+                        </x-jet-danger-button>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+
+    <x-jet-dialog-modal wire:model="open">
+        <x-slot name="title">
+            Editar talla
+        </x-slot>
+
+        <x-slot name="content">
+            <x-jet-label>
+                Talla
+            </x-jet-label>
+
+            <x-jet-input wire:model="name_edit" type="text" class="w-full" />
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open', false)">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-button wire:click="update" wire:loading.attr="disabled" wire:target="update">
+                Actualizar
+            </x-jet-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
