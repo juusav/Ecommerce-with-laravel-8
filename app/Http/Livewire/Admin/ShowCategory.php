8559d2a25d1706a9d6ff89e1d3c8053496b3a9b9
Justice Velasco <justicevgomez001@gmail.com>
2021-07-31 19:03:43 +0200

    show subcategories en Admin view

--- app/Http/Livewire/Admin/ShowCategory.php
+++ app/Http/Livewire/Admin/ShowCategory.php
@@ -3,14 +3,52 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Category;
+use App\Models\Subcategory;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class ShowCategory extends Component{
 
-    public $category;
+    public $category, $subcategories;
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug', //Debe ser unico en la tabla categories campo slug
+        'createForm.color' => 'required',
+        'createForm.size' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+    ];
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'color' => false,
+        'size' => false,
+    ];
 
     public function mount(Category $category){
         $this->category = $category;
+        $this->getSubcategories();
+    }
+
+    public function updatedCreateFormName($value){
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function getSubcategories(){
+        $this->subcategories = Subcategory::where('category_id', $this->category->id)->get();
+    }
+
+    public function save(){
+        $this->validate();
+    }
+
+    public function edit(Subcategory $subcategory){
+         
     }
 
     public function render(){
--- resources/views/livewire/admin/show-category.blade.php
+++ resources/views/livewire/admin/show-category.blade.php
@@ -1,3 +1,112 @@
-<div>
+<div class="container py-12">
+    <x-jet-form-section submit="save" class="mb-6">
+        <x-slot name="title">
+            Crear subcategoria
+        </x-slot>
 
+        <x-slot name="description"></x-slot>
+
+        <x-slot name="form">
+            {{-- Name --}}
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label>Nombre</x-jet-label>
+                <x-jet-input type="text" class="w-full mt-1" wire:model="createForm.name" />
+
+                <x-jet-input-error for="createForm.name" />
+            </div>
+            {{-- Slug --}}
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label>Slug</x-jet-label>
+                <x-jet-input disabled type="text" class="w-full mt-1 bg-gray-100" wire:model="createForm.slug" />
+
+                <x-jet-input-error for="createForm.slug" />
+            </div>
+            {{-- Size --}}
+            <div class="col-span-6 sm:col-span-4">
+                <div class="flex items-center">
+                    <p>¿Esta subcategoría necesita especificar talla?</p>
+
+                    <div class="ml-auto">
+                        <label>
+                            <input wire:model.defer="createForm.size" type="radio" name="size" value="1">
+                            Si
+                        </label>
+
+                        <label>
+                            <input wire:model.defer="createForm.size" type="radio" name="size" value="0">
+                            No
+                        </label>
+                    </div>
+                    <x-jet-input-error for="createForm.size" />
+                </div>
+            </div>
+            {{-- Color --}}
+            <div class="col-span-6 sm:col-span-4">
+                <div class="flex items-center">
+                    <p>¿Esta subcategoría necesita especificar color?</p>
+
+                    <div class="ml-auto">
+                        <label>
+                            <input wire:model.defer="createForm.color" type="radio" name="color" value="1">
+                            Si
+                        </label>
+
+                        <label>
+                            <input wire:model.defer="createForm.color" type="radio" name="color" value="0">
+                            No
+                        </label>
+                    </div>
+                </div>
+                <x-jet-input-error for="createForm.color" />
+            </div>
+            
+        </x-slot>
+
+        <x-slot name="actions">
+            <x-jet-action-message class="mr-3" on="saved">Categoría creada</x-jet-action-message>
+            <x-jet-button>Crear categoria</x-jet-button>
+        </x-slot>
+    </x-jet-form-section>
+
+    {{-- Edit and delete section --}}
+    <x-jet-action-section>
+        <x-slot name="title">
+            Lista de subcategorías
+        </x-slot>
+
+        <x-slot name="description"></x-slot>
+
+        <x-slot name="content">
+            <table class="text-gray-600">
+                <thead class="border-b border-gray-300">
+                    <tr class="text-left">
+                        <th class="py-2 w-full">Nombre</th>
+                        <th class="py-2">Acción</th>
+                    </tr>
+                </thead>
+
+                {{-- Buttons --}}
+                <tbody class="divide-y divide-gray-300">
+                    @foreach ($subcategories as $subcategory)
+                        <tr>
+                            {{-- Name --}}
+                            <td class="py-2">
+                                <span class="uppercase">{{ $subcategory->name }}</span>
+                            </td>
+
+                            {{-- Edit and delete buttons --}}
+                            <td class="py-2">
+                                <div class="flex divide-x divide-gray-300 font-semibold">
+                                    <a class="pr-2 hover:text-blue-600 cursor-pointer"
+                                        wire:click="edit('{{$subcategory->id}}')">Editar</a>
+                                    <a class="pl-2 hover:text-red-600 cursor-pointer"
+                                        wire:click="$emit('deleteSubcategory', '{{$subcategory->id}}')">Eliminar</a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </x-slot>
+    </x-jet-action-section>
 </div>
